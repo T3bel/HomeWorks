58b49c5dd4a385f6277962ef0205bde3de634802
Dmitry <147510986+T3bel@users.noreply.github.com>
2025-02-26 15:43:38 +0300

    1

---
+++ Task3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            inputValuePoint:
+            if (!int.TryParse(Console.ReadLine(),out int a) | 
+                !int.TryParse(Console.ReadLine(), out int b))
+            {
+
+                Console.WriteLine("Это не число, попробуйте ещё раз");
+                goto inputValuePoint;
+            }
+
+
+            if (a > b) Console.WriteLine($"Число {a} больше чем {b}");
+            else if (a < b) Console.WriteLine($"Число {a} меньше чем {b}");
+            else  Console.WriteLine($"Число {a} равняется числу {b}");
+        }
+    }
+}
