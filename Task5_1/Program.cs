aedc4acf532732745ca65f6b687f1f58e0dc752b
Dmitry <147510986+T3bel@users.noreply.github.com>
2025-03-13 09:45:31 +0300

    Выполнены задачи с 1-й по 7-ю

---
+++ Task5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            var random = new Random();
+            int[] numArray = new int[n];
+            int evenCounter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                numArray[i] = random.Next(0, 101);
+                evenCounter += numArray[i] % 2 == 0 ? 1 : 0;
+                Console.Write($"{numArray[i]} ");
+            }
+
+            string outputText = evenCounter > numArray.Length / 2 ? "Четных чисел больше чем нечетных" : 
+                evenCounter == numArray.Length / 2 ? "Количество четных и нечетных чисел равно" : "Нечетных чисел больше чем четных";
+
+            Console.WriteLine($"\n{outputText}");
+            Console.ReadKey();
+
+        }
+    }
+}
