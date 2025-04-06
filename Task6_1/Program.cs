b53b836caa3768910c17facbf413f4c45dc659a0
Dmitry <147510986+T3bel@users.noreply.github.com>
2025-04-06 19:37:30 +0300

    Выполнены задачи 6 урока

---
+++ Task6_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = Console.ReadLine();
+            var splitText = Regex.Split(inputText.Trim(),@"\s+");
+
+            Array.Sort(splitText);
+            Console.WriteLine(splitText[splitText.Length - 1]);
+            Console.ReadKey();
+            //for (int i = 0; i < splitText.Length; i++)
+            //{
+            //    if ( splitText[i].Length > maxLenght)
+            //    {
+            //        string value = "";
+            //        value += splitText[i];
+            //        maxLenght= splitText[i].Length;
+            //    }
+
+            //}
+           
+        }
+    }
+}
